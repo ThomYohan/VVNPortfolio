6c2b1817e69e9434a9bb31259c6f9289d66f8d78
Thomas Yohannes <thomasgyohannes@gmail.com>
2023-04-21 17:34:46 -0500

    cleaning up commented out/placeholder code

--- src/router.ts
+++ src/router.ts
@@ -1,30 +1,3 @@
-// import { createRouter, createWebHistory } from 'vue-router'
-// import Home from '../views/Home.vue'
-
-// const routes = [
-//     { path: '/', component: Home },
-//     // { path: '/about', component: About },
-//   ]
-  
-//   // 3. Create the router instance and pass the `routes` option
-//   // You can pass in additional options here, but let's
-//   // keep it simple for now.
-//   const router = VueRouter.createRouter({
-//     // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-//     history: VueRouter.createWebHashHistory(),
-//     routes, // short for `routes: routes`
-//   })
-  
-//   // 5. Create and mount the root instance.
-//   const app = Vue.createApp({})
-//   // Make sure to _use_ the router instance to make the
-//   // whole app router-aware.
-//   app.use(router)
-  
-//   app.mount('#app')
-
-//   export default router
-
 import { createRouter, createWebHistory } from 'vue-router'
 // import Home from '@/views/Home.vue'
 import Home from './views/Home.vue'

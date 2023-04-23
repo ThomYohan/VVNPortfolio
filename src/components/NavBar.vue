09a9eb0411df34f3953c6f8fe1ace39c31f3b317
Thomas Yohannes <thomasgyohannes@gmail.com>
2023-04-23 02:35:16 -0500

    data/styling changes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,34 +1,29 @@
 <template>
     <!-- <n-config-provider :theme="darkTheme"> -->
-    <n-space>
-        <n-switch size="medium">
-            <template #icon>
-                🤔
-            </template>
-        </n-switch>
-    </n-space>
-    <div class="navBar" v-for="data in Data.routes" :key="data.id">
-        <!-- <n-button @click="theme = darkTheme">
-            Dark
-        </n-button>
-        <n-button @click="theme = null">
-            Light
-        </n-button> -->
-        <!-- <n-menu> -->
-            <router-link :to="data.route">{{ data.name }}</router-link>
-        <!-- </n-menu> -->
-        <!-- <router-link to="/">
-            <h2>Home</h2>
-        </router-link>
-        <router-link to="/About">
-            <h2>About</h2>
-        </router-link>
-        <router-link to="/Projects">
-            <h2>Projects</h2>
-        </router-link>
-        <router-link to="/Resume">
-            <h2>Resume</h2>
-        </router-link> -->
+    <div class="navBar">
+        <div>
+            <h3>Orca</h3>
+        </div>
+        <div class="row">
+            <div class="navBarTile" v-for="data in Data.routes" :key="data.id">
+                <!-- <n-button @click="theme = darkTheme">
+                        Dark
+                    </n-button>
+                    <n-button @click="theme = null">
+                        Light
+                    </n-button> -->
+                <!-- <n-menu> -->
+                <router-link class="jdNav" :to="data.route">{{ data.name }}</router-link>
+                <!-- </n-menu> -->
+            </div>
+            <n-space>
+                <n-switch size="medium">
+                    <template #icon>
+                        🤔
+                    </template>
+                </n-switch>
+            </n-space>
+        </div>
     </div>
     <!-- </n-config-provider> -->
 </template>
--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -1,34 +1,38 @@
 <template>
-    <div v-for="data in Data.projects" :key="data.id">
-        <!-- <n-image width="200" height="100" src="../../public/GBAPurpleStack.png" /> -->
-        <h3>{{ data.name }}</h3>
-        <h4>{{ data.subheading }}</h4>
-        <!-- <n-image :src="data.thumbnail" /> -->
+    <div class="tileList">
+        <div class="tile" v-for="data in Data.projects" :key="data.id">
+            <!-- <n-image width="200" height="100" src="../../public/GBAPurpleStack.png" /> -->
+            <h3>{{ data.name }}</h3>
+            <h4>{{ data.subheading }}</h4>
+            <n-image class="tileImg" :src="data.thumbnail" />
+        </div>
+
+        
+        <!-- <n-grid :cols="2">
+            <n-form-item-gi label="Item Count">
+                <n-input-number :min="1" />
+            </n-form-item-gi>
+            <n-form-item-gi label="Max Rows After Collapsed">
+                <n-input-number :min="1" />
+            </n-form-item-gi>
+            <n-form-item-gi label="Show Suffix Node">
+                <n-switch />
+            </n-form-item-gi>
+            <n-form-item-gi label="Grid Collapsed">
+                <n-switch />
+            </n-form-item-gi>
+        </n-grid> -->
+
+
+        <!-- <n-grid :cols="5" :collapsed="gridCollapsed" :collapsed-rows="gridCollapsedRows">
+            <n-gi v-for="i in gridItemCount" :key="i" :class="i % 2 ? 'green' : 'light-green'">
+                {{ i }}
+            </n-gi>
+            <n-gi v-if="showSuffix" suffix class="suffix" #="{ overflow }">
+                {{ overflow ? 'Node Overflows Exists' : 'No Node Overflows' }}
+            </n-gi>
+        </n-grid> -->
     </div>
-    <n-grid :cols="2">
-        <n-form-item-gi label="Item Count">
-            <!-- <n-input-number v-model:value="gridItemCount" :min="1" /> -->
-            <n-input-number :min="1" />
-        </n-form-item-gi>
-        <n-form-item-gi label="Max Rows After Collapsed">
-            <n-input-number :min="1" />
-        </n-form-item-gi>
-        <n-form-item-gi label="Show Suffix Node">
-            <!-- <n-switch v-model:value="showSuffix" /> -->
-            <n-switch />
-        </n-form-item-gi>
-        <n-form-item-gi label="Grid Collapsed">
-            <n-switch />
-        </n-form-item-gi>
-    </n-grid>
-    <!-- <n-grid :cols="5" :collapsed="gridCollapsed" :collapsed-rows="gridCollapsedRows">
-        <n-gi v-for="i in gridItemCount" :key="i" :class="i % 2 ? 'green' : 'light-green'">
-            {{ i }}
-        </n-gi>
-        <n-gi v-if="showSuffix" suffix class="suffix" #="{ overflow }">
-            {{ overflow ? 'Node Overflows Exists' : 'No Node Overflows' }}
-        </n-gi>
-    </n-grid> -->
 </template>
 
 
@@ -41,14 +45,14 @@ export default {
             Data
         }
     },
-    setup () {
-    return {
-    //   gridCollapsed: ref(false),
-    //   gridCollapsedRows: ref(1),
-    //   gridItemCount: ref(4),
-    //   showSuffix: ref(true)
+    setup() {
+        return {
+            //   gridCollapsed: ref(false),
+            //   gridCollapsedRows: ref(1),
+            //   gridItemCount: ref(4),
+            //   showSuffix: ref(true)
+        }
     }
-  }
 }
 </script>
 

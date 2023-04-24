62e5826a962bdc7ef67730c54aab186168182e98
Thomas Yohannes <thomasgyohannes@gmail.com>
2023-04-24 02:58:36 -0500

    animated landing section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,12 @@
 <template>
     <!-- <n-config-provider :theme="!darkTheme"> -->
     <br>
-    <div class="jdLandingContainer">
+    <!-- <div class="jdLandingContainer">
         <div>
             <h1 class="jdH1">Hello I'm Thomas</h1>
             <div>
-                <n-gradient-text class="jdH1" gradient="linear-gradient(90deg, red 0%, green 50%, blue 100%)">
+                <n-gradient-text class="jdH1" 
+                gradient="linear-gradient(90deg, #E3B23C, #44FFD2, #5F1A37)">
                     A Frontend Software Engineer
                 </n-gradient-text>
                 <h1 class="jdH1"> based in Wichita, Kansas</h1>
@@ -17,14 +18,49 @@
         <n-button strong secondary type="warning">
             Resume
         </n-button>
-        <!-- <h3></h3> -->
+    </div> -->
+    <div class="jdLandingContainer">
+        <div>
+            <Motion :initial="{ x: -50, opacity: 0 }" :animate="{ x: 0, opacity: 1 }" :transition="{ duration: 0.5 }">
+                <h1 class="jdH1">Hey! I'm Thomas</h1>
+            </Motion>
+            <div>
+                <Motion :initial="{ x: -50, opacity: 0 }" :animate="{ x: 0, opacity: 1 }"
+                    :transition="{ duration: 0.5, delay: 0.2 }">
+                    <n-gradient-text class="jdH1" gradient="linear-gradient(90deg, #E3B23C, #44FFD2, #5F1A37)">
+                        A Frontend Software Engineer
+                    </n-gradient-text>
+                </Motion>
+                <Motion :initial="{ x: -50, opacity: 0 }" :animate="{ x: 0, opacity: 1 }"
+                    :transition="{ duration: 0.5, delay: 0.4 }">
+                    <h1 class="jdH1"> based in Wichita, Kansas</h1>
+                </Motion>
+            </div>
+        </div>
+        <div>
+            <Motion :initial="{ x: -50, opacity: 0 }" :animate="{ x: 0, opacity: 1 }"
+                :transition="{ duration: 0.5, delay: 0.6 }">
+                <h2 class="jdH2">Placeholder text, click on the resume</h2>
+            </Motion>
+        </div>
+        <Motion :initial="{ x: -50, opacity: 0 }" :animate="{ x: 0, opacity: 1 }"
+            :transition="{ duration: 0.5, delay: 0.7 }">
+            <n-button strong secondary type="warning">Resume</n-button>
+        </Motion>
+
     </div>
     <div class="aboutContainer">
         <div>
             <h2 class="jdH3">Fullstack Developer with a <h2 class="jdH3">Frontend Focus</h2>
             </h2>
         </div>
-        <h4 class="jdH5">Welcome to my personal website! I'm Thomas Yohannes, a Frontend Software Engineer with a passion for building beautiful and functional web experiences. Using cutting-edge technologies such as Vue 3, Vite, Naive UI, React, HTML, CSS, and TypeScript, I specialize in creating dynamic and engaging websites that captivate users and deliver results. With expertise in Motion One, I can add fluid animations and seamless transitions to elevate your site to the next level. Whether you're looking to build a web app from scratch or need help optimizing an existing site, I'm here to help. Let's collaborate and create something amazing together that reflects the latest trends and best practices in the industry!</h4>
+        <h4 class="jdH5">Welcome to my personal website! I'm Thomas Yohannes, a Frontend Software Engineer with a passion
+            for building beautiful and functional web experiences. Using cutting-edge technologies such as Vue 3, Vite,
+            Naive UI, React, HTML, CSS, and TypeScript, I specialize in creating dynamic and engaging websites that
+            captivate users and deliver results. With expertise in Motion One, I can add fluid animations and seamless
+            transitions to elevate your site to the next level. Whether you're looking to build a web app from scratch or
+            need help optimizing an existing site, I'm here to help. Let's collaborate and create something amazing together
+            that reflects the latest trends and best practices in the industry!</h4>
     </div>
     <Tile />
     <History />
@@ -39,13 +75,14 @@ import { defineComponent } from 'vue'
 // import { Icon } from '@vicons/utils'
 import History from '../components/History.vue'
 import Tile from '../components/Tile.vue'
-import Footer from '../components/Footer.vue'
+import { Motion, Presence } from "motion/vue"
 
 export default defineComponent({
     components: {
         History,
         Tile,
-        Footer
+        Motion,
+        Presence
     },
     setup() {
         return {
